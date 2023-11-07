1692f0da843e46177ba16c6365cdc3742fd3adb5
Oleg <oleglutsik.dev@gmail.com>
2023-11-07 16:18:51 +0200

    first commit

---
+++ lib/main1.dart
@@ -0,0 +1,75 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+class HomePage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              Colors.orange.shade900,
+              Colors.orange.shade800,
+              Colors.orange.shade400,
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeInUp(
+                    duration: Duration(milliseconds: 1000),
+                    child: Text(
+                      "Home page",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  FadeInUp(
+                    duration: Duration(milliseconds: 1300),
+                    child: Text(
+                      "Welcome Back",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Center(
+                    child: Text(
+                      "Login was successful!",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
